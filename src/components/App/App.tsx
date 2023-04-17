89795e0d3d7ab9fa09f84c2f9a8d130b78811438
John-Atha <giannisj3@gmail.com>
2023-04-17 14:01:24 +0300

    Re-organize folder structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Counter } from '../Counter/Counter';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h2">
+        Hello World!
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
